5cba0dc2a866527bd73b8b10205bb664c868bf91
Volodya <volodya.stepanov.edu@yandex.ru>
2017-11-14 16:44:28 +0300

    Тупо скопирован код из урока.

---
+++ app/src/main/java/com/razrabotkin/android/gallery/ImageItem.java
@@ -0,0 +1,34 @@
+package com.razrabotkin.android.gallery;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by PC on 14.11.2017.
+ */
+
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
